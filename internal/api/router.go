7159fe22cb1d149526ccbe3445d0f1557e89f86e
omkar sonawane <omkarsonawane622@gmail.com>
2025-09-24 05:12:54 +0530

    fix(router): change HTTP method for updating subscription line items from POST to PUT for better RESTful compliance

--- internal/api/router.go
+++ internal/api/router.go
@@ -232,7 +232,7 @@ func NewRouter(handlers Handlers, cfg *config.Configuration, logger *logger.Logg
 			subscription.POST("/:id/change/execute", handlers.SubscriptionChange.ExecuteSubscriptionChange)
 
 			// Subscription line item management
-			subscription.POST("/lineitems/:id", handlers.Subscription.UpdateSubscriptionLineItem)
+			subscription.PUT("/lineitems/:id", handlers.Subscription.UpdateSubscriptionLineItem)
 
 		}
 
