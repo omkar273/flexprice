8667b8c7648456e1f2bb55a6e4d8d13c00d229f4
omkar sonawane <omkarsonawane622@gmail.com>
2026-03-06 14:20:55 +0530

    fix(wallet): update condition for calculating totalPendingCharges to use logical OR

--- internal/service/wallet.go
+++ internal/service/wallet.go
@@ -1232,7 +1232,7 @@ func (s *walletService) GetWalletBalance(ctx context.Context, walletID string) (
 		return nil, err
 	}
 
-	if lo.Contains(w.Config.AllowedPriceTypes, types.WalletConfigPriceTypeAll) && lo.Contains(w.Config.AllowedPriceTypes, types.WalletConfigPriceTypeFixed) {
+	if lo.Contains(w.Config.AllowedPriceTypes, types.WalletConfigPriceTypeAll) || lo.Contains(w.Config.AllowedPriceTypes, types.WalletConfigPriceTypeFixed) {
 		totalPendingCharges = totalPendingCharges.Add(resp.TotalUnpaidAmount)
 	} else {
 		totalPendingCharges = totalPendingCharges.Add(resp.TotalUnpaidUsageCharges).Sub(resp.TotalPaidInvoiceAmount)
@@ -2438,7 +2438,7 @@ func (s *walletService) GetWalletBalanceV2(ctx context.Context, walletID string)
 		return nil, err
 	}
 
-	if lo.Contains(w.Config.AllowedPriceTypes, types.WalletConfigPriceTypeAll) && lo.Contains(w.Config.AllowedPriceTypes, types.WalletConfigPriceTypeFixed) {
+	if lo.Contains(w.Config.AllowedPriceTypes, types.WalletConfigPriceTypeAll) || lo.Contains(w.Config.AllowedPriceTypes, types.WalletConfigPriceTypeFixed) {
 		totalPendingCharges = totalPendingCharges.Add(resp.TotalUnpaidAmount)
 	} else {
 		totalPendingCharges = totalPendingCharges.Add(resp.TotalUnpaidUsageCharges).Sub(resp.TotalPaidInvoiceAmount)
@@ -2604,7 +2604,7 @@ func (s *walletService) GetWalletBalanceFromCache(ctx context.Context, walletID 
 		return nil, err
 	}
 
-	if lo.Contains(w.Config.AllowedPriceTypes, types.WalletConfigPriceTypeAll) && lo.Contains(w.Config.AllowedPriceTypes, types.WalletConfigPriceTypeFixed) {
+	if lo.Contains(w.Config.AllowedPriceTypes, types.WalletConfigPriceTypeAll) || lo.Contains(w.Config.AllowedPriceTypes, types.WalletConfigPriceTypeFixed) {
 		totalPendingCharges = totalPendingCharges.Add(resp.TotalUnpaidAmount)
 	} else {
 		totalPendingCharges = totalPendingCharges.Add(resp.TotalUnpaidUsageCharges).Sub(resp.TotalPaidInvoiceAmount)
