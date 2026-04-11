ae91b28a3d5450f5024b128ce7525bb8baf8ca3b
omkar sonawane <omkarsonawane622@gmail.com>
2026-04-11 16:55:56 +0530

    chore: remove outdated ParseGeminiPricing documentation and delete unused ONETIME billing period migration

--- migrations/postgres/V3_onetime_billing_period.up.sql
+++
@@ -1,6 +0,0 @@
--- Normalize all one-time prices to use billing_period=ONETIME instead of billing_cadence=ONETIME.
--- After this migration every price row has billing_cadence='RECURRING'.
-UPDATE prices
-SET billing_cadence = 'RECURRING',
-    billing_period  = 'ONETIME'
-WHERE billing_cadence = 'ONETIME';
