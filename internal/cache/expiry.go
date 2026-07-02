ddfdfe8c6bc1635a0cf0c9b34fd9249ce403c21d
Gursimar10 <gursimar10111@gmail.com>
2026-07-02 13:37:22 +0530

    feat(redis): rollback to old ExpiryWalletBalance

--- internal/cache/expiry.go
+++ internal/cache/expiry.go
@@ -5,7 +5,7 @@ import "time"
 const (
 	ExpiryDefaultInMemory  = 2 * time.Minute
 	ExpiryDefaultRedis     = 30 * time.Minute
-	ExpiryWalletBalance    = 2 * time.Minute
+	ExpiryWalletBalance    = 30 * time.Minute
 	ExpiryWalletAlertCheck = 1 * time.Minute
 	ExpiryPriceSyncLock    = 2 * time.Hour
 )
